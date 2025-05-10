4c665298ebf569c50bc74fa449d03cac75b1cdb9
hanif404 <hanif.kharismadini.04@gmail.com>
2025-05-10 22:32:53 +0700

    bugfix: remove comment code

--- resources/js/Pages/User/Registration.vue
+++ resources/js/Pages/User/Registration.vue
@@ -51,29 +51,6 @@
                     </button>
                 </div>
             </div>
-
-            <!-- <div class="form-group">
-                <label for="name">Name</label>
-                <div v-if="errors.name" class="text-danger">{{ errors.name }}</div>
-                <input type="name" v-model="form.name" class="form-control" id="name" placeholder="Enter name">
-            </div>
-            <div class="form-group">
-                <label for="email">Email</label>
-                <div v-if="errors.email" class="text-danger">{{ errors.email }}</div>
-                <input type="email"  v-model="form.email" class="form-control" id="email" placeholder="Enter email">
-            </div>
-            <div class="form-group">
-                <label for="password">Password</label>
-                <div v-if="errors.password" class="text-danger">{{ errors.password }}</div>
-                <input type="password" v-model="form.password" class="form-control" id="password" placeholder="Password">
-            </div>
-            <div class="form-group">
-                <label for="password_confirmation">Confirm Password</label>
-                <div v-if="errors.password" class="text-danger">{{ errors.password_confirmation }}</div>
-                <input type="password" v-model="form.password_confirmation" class="form-control" id="password_confirmation" placeholder="Confirm Password">
-            </div>
-
-            <button type="submit">Register</button> -->
         </form>
     </div>
 </template>
